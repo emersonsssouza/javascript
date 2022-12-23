f01dcd75eab25cedb305f1c41f77ffbba066f043
emerson souza <emerson-santos1940@hotmail.com>
2022-12-22 23:35:51 -0300

    att

---
+++ teste-JS/script.js
@@ -0,0 +1,28 @@
+var hora = new Date().getHours();
+
+if (hora < 12)
+alert("Bom dia ");
+
+ if (hora >= 12 && hora <18)
+ alert("Boa tarde");
+ 
+   if (hora >=18)
+   alert("Boa noite");
+
+   // Fim do primeiro script (pergunta)
+  
+ document.write(`A hora agora é `+ new Date())
+
+ var testedata = new date().getDate();
+ 
+ console.log(testedata)
+ console.log("A variavel testdata é do tipo: " + typeof(testedata));
+
+ // fim do segundo script
+
+ var n1 = 5 
+ var n2 = 10
+ var n3 = 25 
+ 
+
+ 
